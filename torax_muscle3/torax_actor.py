5d219941c27b72ce6133bdd694d72a6d9c8f2cee
MateoBell <mateo.bellouard@gmail.com>
2026-06-02 16:02:38 +0200

    Modify orchestration of last_equilibrium_call

--- torax_muscle3/torax_actor.py
+++ torax_muscle3/torax_actor.py
@@ -40,6 +40,7 @@ from torax._src.geometry.imas import IMASConfig
 from torax._src.geometry.pydantic_model import GeometryConfig
 from torax._src.imas_tools.input.core_sources import sources_from_IMAS
 from torax._src.imas_tools.input.core_profiles import profile_conditions_from_IMAS
+from torax._src.imas_tools.input.core_sources import sources_from_IMAS
 from torax._src.imas_tools.output.core_profiles import core_profiles_to_IMAS
 from torax._src.imas_tools.output.equilibrium import torax_state_to_imas_equilibrium
 from ymmsl import Operator
@@ -90,7 +91,10 @@ class ToraxMuscleRunner:
     """Whether the run_sim function has been run fully"""
     last_equilibrium_call = -np.inf
     """Last timestamp for which the MUSCLE3 communication was done"""
-
+    last_core_profiles_call = -np.inf
+    """Last timestamp for which the MUSCLE3 communication was done"""
+    last_core_sources_call = -np.inf
+    """Last timestamp for which the MUSCLE3 communication was done"""
     def __init__(self) -> None:
         self.get_instance()
         self.extra_var_col = ExtraVarCollection()
@@ -159,7 +163,8 @@ class ToraxMuscleRunner:
     def run_o_i(self) -> None:
         """Send out time loop state using MUSCLE3 connections"""
         self.t_next_inner = self.get_t_next()
-        if self.t_cur >= self.last_equilibrium_call + self.equilibrium_interval:
+        # if self.t_cur >= self.last_equilibrium_call + self.equilibrium_interval:
+        if self.t_cur >= self.last_core_sources_call + self.equilibrium_interval:
             if self.instance.is_connected("equilibrium_o_i"):
                 self.send_ids(self.get_equilibrium_ids(), "equilibrium", "o_i")
             if self.instance.is_connected("core_profiles_o_i"):
@@ -167,7 +172,8 @@ class ToraxMuscleRunner:
 
     def run_s(self) -> None:
         """Update time loop state using MUSCLE3 connections"""
-        if self.t_cur >= self.last_equilibrium_call + self.equilibrium_interval:
+        # if self.t_cur >= self.last_equilibrium_call + self.equilibrium_interval:
+        if self.t_cur >= self.last_core_sources_call + self.equilibrium_interval:
             self.receive_equilibrium(port_name="s")
             self.receive_core_profiles(port_name="s")
             self.receive_core_sources(port_name="s")
@@ -191,8 +197,9 @@ class ToraxMuscleRunner:
             return
 
         if self.output_all_timeslices:
-            self.db_out.put_slice(self.get_equilibrium_ids())
-            self.db_out.put_slice(self.get_core_profiles_ids())
+            if self.t_cur >= self.last_core_sources_call + self.equilibrium_interval:
+                self.db_out.put_slice(self.get_equilibrium_ids())
+                self.db_out.put_slice(self.get_core_profiles_ids())
 
     def run_o_f(self) -> None:
         """Send out final state using MUSCLE3 connections"""
@@ -318,10 +325,37 @@ class ToraxMuscleRunner:
             and core_profiles_data.code.output_flag[0] == -1
         ):
             return
-
+        self.last_core_profiles_call = self.t_cur
         core_profiles_conditions = profile_conditions_from_IMAS(core_profiles_data)
         self.torax_config.update_fields(
             {"profile_conditions": core_profiles_conditions}
+        )
+        self.runtime_params_provider = RuntimeParamsProvider.from_config(
+            self.torax_config
+        )
+    
+    def receive_core_sources(self, port_name: str) -> None:
+        """Receive core_sources IDS through MUSCLE3 connections"""
+        if not self.instance.is_connected(f"core_sources_{port_name}"):
+            return
+        core_sources_data, self.t_cur, t_next = self.receive_ids_data(
+            "core_sources", port_name
+        )
+        self.update_t_next(t_next, port_name)
+        self.last_core_sources_call = self.t_cur
+        # ignore this entry if input source didn't converge
+        if (
+            core_sources_data.code.output_flag
+            and core_sources_data.code.output_flag[0] == -1
+        ):
+            return
+
+        sources = sources_from_IMAS(core_sources_data)
+        # Currently creates problem with icrh: tries to load TORIC. See why.
+        # del sources['icrh']
+        # exit()
+        self.torax_config.update_fields(
+            {f"sources.{key}": value for key, value in sources.items()}
         )
         self.runtime_params_provider = RuntimeParamsProvider.from_config(
             self.torax_config
