37930cf83e224694052ef2f312133ed7ad7d6146
Mike Sanders <msanders@ignitioncomputing.com>
2026-06-15 14:37:13 +0200

    checkpointing working

--- torax_muscle3/torax_actor.py
+++ torax_muscle3/torax_actor.py
@@ -12,12 +12,14 @@ Last (for sure) compatible torax commit: 4b76ef0566
 
 import logging
 from typing import Optional, Tuple
+import os
+import pathlib
 
 import numpy as np
 from imas import DBEntry, IDSFactory
 from imas.ids_defs import CLOSEST_INTERP
 from imas.ids_toplevel import IDSToplevel
-from libmuscle import Instance, Message
+from libmuscle import Instance, InstanceFlags, Message
 from torax import experimental as torax_experimental
 from torax import (
     build_torax_config_from_file,
@@ -42,7 +44,10 @@ from torax._src.imas_tools.input.core_sources import sources_from_IMAS
 from torax._src.imas_tools.input.core_profiles import profile_conditions_from_IMAS
 from torax._src.imas_tools.output.core_profiles import core_profiles_to_IMAS
 from torax._src.imas_tools.output.equilibrium import torax_state_to_imas_equilibrium
+from torax._src.simulation_app import _write_simulation_output_to_dir
+from torax._src.output_tools.output import StateHistory
 from ymmsl import Operator
+import inspect
 
 from torax_muscle3.utils import (
     ExtraVarCollection,
@@ -50,17 +55,55 @@ from torax_muscle3.utils import (
     get_setting_optional,
     merge_extra_vars,
     create_light_equilibrium,
+    get_port_list,
 )
 
 logger = logging.getLogger()
 
+import enum
+
+def serialize_equilibrium_objects(data, key=None):
+    if key == 'equilibrium_object':
+        return serialize_equilibrium_objects(data.serialize())
+    elif isinstance(data, enum.Enum):
+        return data.value
+    elif inspect.isgenerator(data):
+        return serialize_equilibrium_objects(list(data))
+    elif isinstance(data, dict):
+        return {
+            str(k): serialize_equilibrium_objects(v, key=k)
+            for k, v in data.items()
+        }
+    elif isinstance(data, list):
+        return [serialize_equilibrium_objects(item) for item in data]
+    elif isinstance(data, tuple):
+        return tuple(serialize_equilibrium_objects(item) for item in data)
+    elif isinstance(data, pathlib.PosixPath):
+        return str(data)
+    return data
+
+def deserialize_equilibrium_objects(data, key=None):
+    if key == 'equilibrium_object':
+        eq = IDSFactory().equilibrium()
+        eq.deserialize(data)
+        return eq
+    elif isinstance(data, dict):
+        return {
+            float(k) if k.isnumeric() else k: deserialize_equilibrium_objects(v, key=k)
+            for k, v in data.items()
+        }
+    elif isinstance(data, list):
+        return [deserialize_equilibrium_objects(item) for item in data]
+    elif isinstance(data, tuple):
+        return tuple(deserialize_equilibrium_objects(item) for item in data)
+    return data
 
 class ToraxMuscleRunner:
     """Object for running torax simulation"""
 
     first_run: bool = True
     """Whether this is the first reuse_instance loop"""
-    output_all_timeslices: Optional[bool] = False
+    output_all_timeslices: bool = False
     """Whether to combine all timeslices at once in the final output"""
     db_out: DBEntry
     """IMAS DBEntry for gathering the timeslices if output_all_timeslices is True"""
@@ -82,6 +125,8 @@ class ToraxMuscleRunner:
     """Object to save state of IDS variables that cannot be saved in Torax state """
     t_cur: float
     """Time value inside time loop"""
+    t_final: float
+    """Last time value of simulation"""
     t_next_inner: Optional[float] = None
     """Next expected output timestamp for inner time loop"""
     t_next_outer: Optional[float] = None
@@ -101,20 +146,122 @@ class ToraxMuscleRunner:
             raise RuntimeError("Already finished")
 
         while self.instance.reuse_instance():
-            if self.first_run:
-                self.run_prep()
-            self.run_f_init()
+            if self.instance.resuming():
+                self.run_resume()
+            if self.instance.should_init():
+                if self.first_run:
+                    self.run_prep()
+                self.run_f_init()
             while not self.step_fn.is_done(self.t_cur):
                 self.run_o_i()
                 self.run_s()
                 self.run_timestep()
                 if self.finished:
                     break
+                self.save_snapshot()
             self.run_o_f()
+            self.save_final_snapshot()
 
         self.finished = True
 
-    def run_prep(self) -> None:
+    def run_resume(self) -> None:
+        # receive message
+        msg = self.instance.load_snapshot()
+        # unpack class vars
+        self.first_run = msg.data[0]["first_run"]
+        self.t_cur = msg.data[0]["t_cur"]
+        self.t_final = msg.data[0]["t_final"]
+        self.t_next_inner = msg.data[0]["t_next_inner"]
+        self.t_next_outer = msg.data[0]["t_next_outer"]
+        self.finished = msg.data[0]["finished"]
+        self.last_communication = msg.data[0]["last_communication"]
+        beep = deserialize_equilibrium_objects(msg.data[0]["torax_config"])
+        self.torax_config = ToraxConfig.from_dict(
+            deserialize_equilibrium_objects(msg.data[0]["torax_config"])
+        )
+        # unpack netcdf path, deserialize and add to class vars
+        self.torax_config.update_fields(
+            {
+                "restart": {
+                    "filename": msg.data[0]["netcdf_path"],
+                    "time": self.t_cur,
+                    "do_restart": True,
+                    "stitch": True,
+                }
+            }
+        )
+        # run_prep with given torax_config (add optional arg)
+        self.run_prep(self.torax_config)
+        self.extra_var_col = ExtraVarCollection.model_validate(
+            msg.data[0]["extra_var_col"]
+        )
+        if self.output_all_timeslices:
+            self.db_out = DBEntry("imas:memory?path=/db_out/", "w")
+            for ids_name, obj in msg.data[0]["db_out"].items():
+                ids = self.db_out.factory.new(ids_name)
+                ids.deserialize(obj)
+                self.db_out.put(ids)
+        self.sim_state, self.post_processed_outputs = (
+            get_initial_state_and_post_processed_outputs(
+                step_fn=self.step_fn,
+                geometry_overrides=self.geometry_provider,
+                runtime_params_overrides=self.runtime_params_provider,
+            )
+        )
+
+    def save_snapshot(self) -> None:
+        if self.instance.should_save_snapshot(self.t_cur):
+            # store class vars
+            # save sim_state and state history to netcdf, store path
+            sim_error = self.step_fn.check_for_errors(
+                self.sim_state,
+                self.post_processed_outputs,
+            )
+            config_module_str = self.instance.get_setting("python_config_module")
+            my_torax_config = build_torax_config_from_file(
+                path=config_module_str,
+            )
+            data_tree = StateHistory(
+                state_history=[self.sim_state],
+                post_processed_outputs_history=[self.post_processed_outputs],
+                sim_error=sim_error,
+                # torax_config=self.torax_config,
+                torax_config=my_torax_config,
+            ).simulation_output_to_xr()
+            output_dir = "blabla"
+            output_file = _write_simulation_output_to_dir(output_dir, data_tree)
+            output_file = os.path.abspath(output_file)
+            # store current torax_config
+            # send message
+            my_torax_config = serialize_equilibrium_objects(self.torax_config.model_dump())
+            data = [{
+                "first_run": bool(self.first_run),
+                "t_cur": float(self.t_cur),
+                "t_final": float(self.t_final),
+                "t_next_inner": float(self.t_next_inner) if self.t_next_inner is not None else None,
+                "t_next_outer": float(self.t_next_outer) if self.t_next_outer is not None else None,
+                "finished": self.finished,
+                "last_communication": float(self.last_communication) if self.last_communication is not None else None,
+                "netcdf_path": output_file,
+                "torax_config": my_torax_config,
+                "extra_var_col": self.extra_var_col.model_dump(),
+                "db_out": {
+                    ids_name: self.db_out.get(ids_name).serialize()
+                    for ids_name in [
+                        port.replace("_o_f", "")
+                        for port in get_port_list(self.instance, Operator.O_F)
+                    ]
+                },
+            }]
+            msg = Message(float(self.t_cur), data=data)
+            self.instance.save_snapshot(msg)
+
+    def save_final_snapshot(self) -> None:
+        if self.instance.should_save_final_snapshot():
+            msg = Message(float(self.t_cur))
+            self.instance.save_final_snapshot(msg)
+
+    def run_prep(self, torax_config: Optional[ToraxConfig] = None) -> None:
         """Prepare a TORAX simulation based on torax config and MUSCLE3 settings"""
         self.communication_interval = get_setting_optional(
             self.instance, "communication_interval", 1e-6
@@ -124,16 +271,20 @@ class ToraxMuscleRunner:
         )
         if self.output_all_timeslices:
             self.db_out = DBEntry("imas:memory?path=/db_out/", "w")
-        # load config file from path
-        config_module_str = self.instance.get_setting("python_config_module")
-        self.torax_config = build_torax_config_from_file(
-            path=config_module_str,
-        )
+        if torax_config is None:
+            # load config file from path
+            config_module_str = self.instance.get_setting("python_config_module")
+            self.torax_config = build_torax_config_from_file(
+                path=config_module_str,
+            )
+        else:
+            self.torax_config = torax_config
         self.fix_ymmsl_settings()
         self.geometry_provider = self.torax_config.geometry.build_provider
         self.runtime_params_provider = RuntimeParamsProvider.from_config(
             self.torax_config
         )
+        self.step_fn = make_step_fn(self.torax_config)
 
     def run_f_init(self) -> None:
         """Initialize the actor state before the time loop using MUSCLE3 connections"""
@@ -220,7 +371,8 @@ class ToraxMuscleRunner:
                 Operator.O_I: [f"{ids_name}_o_i" for ids_name in coupled_ids_names],
                 Operator.S: [f"{ids_name}_s" for ids_name in coupled_ids_names],
                 Operator.O_F: [f"{ids_name}_o_f" for ids_name in coupled_ids_names],
-            }
+            },
+            flags=InstanceFlags.USES_CHECKPOINT_API,
         )
 
     def get_equilibrium_ids(self) -> IDSToplevel:
@@ -298,12 +450,23 @@ class ToraxMuscleRunner:
                 )
         # temp extra vars code
         self.extra_var_col.pad_extra_vars()
-        self.geometry_provider = torax_experimental.geometry.Geometry.from_dict(
+
+        # self.geometry_provider = torax_experimental.geometry.Geometry.from_dict(
+        #     {
+        #         "geometry_type": geometry.GeometryType.IMAS,
+        #         "geometry_configs": geometry_configs,
+        #     }
+        # ).build_provider
+
+        self.torax_config.update_fields(
             {
-                "geometry_type": geometry.GeometryType.IMAS,
-                "geometry_configs": geometry_configs,
+                "geometry": {
+                    "geometry_type": geometry.GeometryType.IMAS,
+                    "geometry_configs": geometry_configs,
+                }
             }
-        ).build_provider
+        )
+        self.geometry_provider = self.torax_config.geometry.build_provider
 
     def receive_core_profiles(self, port_name: str) -> None:
         """Receive core_profiles IDS through MUSCLE3 connections"""
